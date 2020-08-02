cc21a0ace4d8b2195a6cfb9ea16a74100b27a9b0
Kenya Chan <kenya.chan@gmail.com>
2020-08-02 16:21:54 +1000

    Updates

---
+++ precache-manifest.7aa8cd6dab8668239ae8c9605819991f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da3a2a117719d969c195d3c94de6f08",
+    "url": "/React-Tutorial/index.html"
+  },
+  {
+    "revision": "93995544f3c0001c3829",
+    "url": "/React-Tutorial/static/css/main.f3900a30.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/React-Tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93995544f3c0001c3829",
+    "url": "/React-Tutorial/static/js/main.733c7e8d.chunk.js"
+  },
+  {
+    "revision": "f4a3984e544610f2b6d1",
+    "url": "/React-Tutorial/static/js/runtime-main.32939327.js"
+  }
+]);
